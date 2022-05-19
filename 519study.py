c4b3e27cf89c9894cb93d4528ad278820964d407
JungWoo Hao <jwwin7@naver.com>
2022-05-19 17:26:38 +0900

    5/19

---
+++ 519study.py
@@ -0,0 +1,22 @@
+# int  정수형
+# float  실수형
+
+print(-1)
+print(0)   #int
+print(1.1) #float
+print("1+1", 1+1)
+print('2-1', 2-1)
+print('2*1', 2*1)
+print('4/2', 4/2)
+
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4,2)', math.pow(4,2))
+
+import random
+print('random.random()' ,random.random())
+
+
+
+
+
