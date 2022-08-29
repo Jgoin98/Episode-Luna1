0187094f2a4d154cc221c60d5e5f280ae71319fa
jungwoohao <jwwin7@naver.com>
2022-08-29 17:44:26 +0900

    반복문, 반복문 다차원배열

---
+++ 8_07 study.py
@@ -0,0 +1,25 @@
+# 반복문 for 기본형식
+
+names = ['egoing', 'basta', 'blackdew' , 'leezche']
+for name in names:
+    print('Hi,'+name+' . Bye,'+name+'.')
+
+#반복문 다차원 배열
+persons = [
+['egoing', 'Seoul', 'web'],
+['basta', 'Seoul', 'IOT'],
+['blackdew', 'Tongyeong', 'ML'],
+]
+print(persons[0][0])
+
+for person in persons:
+    print(person[0]+','+person[1]+','+person[2])
+
+    person['egoing', 'Seoul', 'web']
+    name = person[0]
+    address = person[1]
+    interest = person[2]
+    print(name, address, interest)
+
+
+
